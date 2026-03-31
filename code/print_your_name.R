06851971fe8efdb88cd6d5c2d249b61bd26c870b
Lucas Mansfield <mansfi79@msu.edu>
2026-03-31 11:12:50 -0400

    small edit to name

--- code/print_your_name.R
+++ code/print_your_name.R
@@ -1,5 +1,5 @@
 # Create an object with your name on it
-lucas <- "linear regression model" #my initials are LRM so 
+lucas_real_name <- "linear regression model" #my initials are LRM so 
 
 # print that object on the console
 print(lucas)
